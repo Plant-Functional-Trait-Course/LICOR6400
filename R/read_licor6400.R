661f93b0c3cc0b633c69f5d927ca9fb40f926a86
richardjtelford <richard.telford@bio.uib.no>
2021-07-29 21:36:55 +0200

    code and data

---
+++ R/read_licor6400.R
@@ -0,0 +1,122 @@
+#' Read LI-COR 6400 data
+#'@param file Path to file to import
+#'
+#'
+#'
+#' @importFrom readr read_delim read_lines
+#' @importFrom dplyr select filter arrange group_by mutate n %>% bind_rows
+#' @importFrom rlang .data
+#' @importFrom tidyr fill
+#' @importFrom tibble tibble
+#' @examples
+#' file <- system.file("0000-AAA8891-210718", package = "LICOR6400")
+#' lic <- read_licor6400(file = file)
+#' @export
+
+
+read_licor6400 <- function(file){
+
+  is_XLS <-grepl("\\.xls$", file)
+  #check if excel file
+  if(is_XLS){
+    tmpdir <- tempdir()
+    newfile <- file.path(
+      tmpdir,
+      "licor",
+      gsub("xls$", "csv", basename(file))
+    )
+    system(paste(system.file("unoconv", package = "LICOR6400"), "-f csv", "-o", newfile, file))
+    file <- newfile
+  }
+
+  ## reading in raw licor data file
+  f <- read_lines(file = file) #min_N = 15
+
+  #find header_row
+  header_row <- grep("^.{0,1}Obs", f)
+  #remove extra header rows
+  if(length(header_row) > 1){
+    f <- f[-header_row[-1]]
+    header_row <- header_row[1]
+  }
+  #check if header row
+  if(length(header_row) == 0){
+    warning("No header row in ", file)
+    return(tibble())
+  }
+
+  #remove everything above header row
+  f <- f[-(1:(header_row - 1))]
+
+  #removing junk  - where licore restarts
+  if(!is_XLS & any(grepl("\\$", f))){
+    remove <- grep("\\$", f)
+    f <- f[-c(remove, remove - 1)]
+    remove <- grep("OPEN", f)
+    f <- f[-c(remove, remove + 1)]
+    remove <- grep("<", f)
+    f <- f[-remove]
+  }
+
+
+  #remove in/out line from excel file
+  in_out <- grep("^in", f)
+  if(length(in_out) > 0){
+    f <- f[-in_out]
+  }
+
+  ##finding rows of metadata
+  meta_rows <- grep("^\"|Remark", f)
+  if(!is_XLS){
+    meta_rows <- meta_rows[-1] #keep header row
+  }
+
+  if(length(meta_rows) == 0){
+    return(tibble())
+  }
+
+  meta <- f[meta_rows]
+  dat <- f[-meta_rows]
+
+  if(length(dat) <= 1){
+    return(tibble())
+  }
+
+  meta <- gsub("\"|Remark=,", "", meta)
+  ## replace multiple spaces with a single space
+  meta <- gsub(" +", " ", meta)
+  ## grouping multiple worded parameters
+  meta <- gsub("CO2 Mixer", "CO2_Mixer", meta)
+  #meta must have arrow
+  meta <- grep("->", meta, value = TRUE)#some rows only have a timestamp
+  #CO2_Mixer -> OFF upsets things remove
+  meta <- grep("CO2_Mixer -> OFF", meta, invert = TRUE, value = TRUE)
+
+
+  #remove comma from csv
+  meta <- gsub(",", "", meta)
+  dat <- gsub(",", "\t", dat)
+
+
+  dat <- read_delim(paste(dat, collapse = "\n"), delim = "\t")
+
+  meta <- read_delim(
+    file = paste(meta, collapse = "\n"),
+    delim = " ",
+    col_names = c("HHMMSS", "variable", "parameter",
+                  "arrow", "level", "unit")) %>%
+    select(-.data$arrow)
+
+  dat <- bind_rows(# bind logged data and metadata
+    meta %>%
+      # discard parameter changes except block temp
+      filter(.data$parameter == "Tblock"),
+    dat) %>%
+    arrange(.data$HHMMSS) %>% # sort data by time logged
+    # fill block temp parameter in for logged data
+    fill(.data$variable, .data$parameter, .data$level, .data$unit) %>%
+    filter(!is.na(.data$Obs)) %>%
+    group_by(.data$level) %>% # group by block temperature
+    mutate(n = n())
+  return(dat)
+}
---
+++ R/recalc_licor.R
@@ -0,0 +1,57 @@
+#' Calculate metrics for LI-COR 6400
+#' @param licor licor object
+#' @details All calculations are from the formulae in the excel file LI-COR6400 exports
+#' @importFrom dplyr %>% select mutate if_else
+#' @importFrom rlang .data
+#' @examples
+#' file <- system.file("0000-AAA8891-210718", package = "LICOR6400")
+#' lic <- read_licor6400(file = file)
+#' #add total leaf area - normally use a join
+#' lic <- lic %>% mutate(total.leaf.area = 6)
+#' calc_licor(licor = lic)
+#' @export
+
+# calculate extra leaf related metrics which scale from basic set of measurements
+calc_licor <- function(licor){
+  licor %>%
+    select(-(.data$Photo:.data$CTleaf),-.data$BLC_1,-.data$BLCond) %>%
+    mutate(
+      BLC_1 = .data$Area * .data$BLCslope + .data$BLCoffst,
+      fda = .data$Flow * 0.000001 / (.data$total.leaf.area * 0.0001),
+      Photo = (.data$CO2R - .data$CO2S * (1000 - .data$H2OR) / (1000 - .data$H2OS)) * .data$fda,
+      Trans = (.data$H2OS - .data$H2OR) / (1000 - .data$H2OS) * .data$fda,
+      Tair_K = .data$Tleaf + 273.15,
+      Twall_K = .data$Tair + 273.15,
+      `R(W/m2)` = (.data$PARi * .data$f_parin + .data$PARo * .data$f_parout) * .data$alphaK,
+      `Tl-Ta` = ((.data$`R(W/m2)` + 0.00000010773 * (.data$Twall_K ^ 4 - .data$Tair_K ^ 4)) -
+                   .data$Trans * 44100) / (.data$BLC_1 * 51.4 + 0.00000043092 * .data$Tair_K ^ 3),
+      CTleaf = .data$Tleaf + .data$`Tl-Ta` * .data$`EBal?`,
+      SVTleaf = 0.61365 * exp(17.502 * .data$CTleaf / (240.97 + .data$CTleaf)),
+      h2o_i = .data$SVTleaf * 1000 / .data$Press,
+      h2odiff = .data$h2o_i - .data$H2OS,
+      CTair = if_else(.data$`EBal?` != 0,
+                      true = .data$Tleaf,
+                      false = (.data$Tair + .data$Tleaf) / 2),
+      SVTair = 0.61365 * exp(17.502 * .data$CTair / (240.97 + .data$CTair)),
+      CndTotal = if_else(.data$h2odiff != 0,
+                         true = (1000 - (.data$h2o_i + .data$H2OS) / 2) / .data$h2odiff * .data$Trans,
+                         false = 0),
+      vp_kPa = .data$H2OS * .data$Press / 1000,
+      VpdA = .data$SVTair - .data$vp_kPa,
+      BLCond = .data$BLC_1 * (.data$StmRat + 1) * (.data$StmRat + 1) /
+        (.data$StmRat * .data$StmRat + 1),
+      Cond = if_else(.data$CndTotal != 0,
+                     true = 1 / (1 / .data$CndTotal - 1 / .data$BLCond),
+                     false = 0),
+      CndCO2 = 1 / (1.6 / .data$Cond + 1.37 / .data$BLCond),
+      Ci = ((.data$CndCO2 - .data$Trans / 2) * .data$CO2S - .data$Photo) /
+        (.data$CndCO2 + .data$Trans / 2),
+      Ci_Pa = .data$Ci * .data$Press * 0.001,
+      `Ci/Ca` = .data$Ci / .data$CO2S,
+      RHsfc = (1 - .data$Trans * .data$Press / .data$SVTleaf / .data$Cond) * 100,
+      C2sfc = .data$CO2S - .data$Photo / (.data$BLCond / 1.35),
+      `AHs/Cs` = .data$Photo * .data$RHsfc / 100 / .data$C2sfc,
+      Trmmol = .data$Trans * 1000,
+      VpdL = .data$SVTleaf - .data$vp_kPa
+    )
+}
